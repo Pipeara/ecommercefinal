1d93e91de34ccad97fb3b7aaea04978d2156e47c
Pipeara <piposo3@hotmail.es>
2024-03-30 11:09:30 -0300

    cambios en login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; // Importa useNavigate desde react-router-dom
 import './Login.css';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
+  const navigate = useNavigate(); // Instancia useNavigate para manejar la navegación
+
   const handleLogin = (e) => {
     e.preventDefault();
-  
-    window.location.href = "/menu"; // Corregido para redirigir correctamente a la página de menú
+
+    // Aquí normalmente se realizaría la lógica de autenticación
+    // Si el inicio de sesión es exitoso, redirigimos al usuario a la página de menú
+    // Usamos navigate('/menu') para navegar a la ruta "/menu"
+    // En este ejemplo, simplemente redirigimos al usuario directamente a "/menu"
+    navigate('/menu');
   };
 
   return (
     <div className="login-container"> 
       <h2>Login</h2>
       <form onSubmit={handleLogin}>
-        <div className="form-group"> {/* Añadido una clase para agrupar los elementos del formulario */}
+        <div className="form-group"> 
           <label htmlFor="email">Email:</label>
           <input
             type="email"
@@ -26,7 +31,7 @@ function Login() {
             required
           />
         </div>
-        <div className="form-group"> {/* Añadido una clase para agrupar los elementos del formulario */}
+        <div className="form-group"> 
           <label htmlFor="password">Contraseña:</label>
           <input
             type="password"
@@ -36,9 +41,9 @@ function Login() {
             required
           />
         </div>
-        <button type="submit" className="login-btn">Ingresar</button> {/* Añadida una clase al botón */}
+        <button type="submit" className="login-btn">Ingresar</button> 
       </form>
-      <p>¿No tienes una cuenta? <Link to="/registro">Regístrate aquí</Link></p> {/* Corregido el enlace a la página de registro */}
+      <p>¿No tienes una cuenta? <Link to="/registro">Regístrate aquí</Link></p>
     </div>
   );
 }
